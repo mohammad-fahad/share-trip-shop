3604d1f6f4ada7f2b9f5c5a08ef4fc6264e88fd4
Mohammad Fahad <fahad@vertis.degital>
2024-10-31 03:44:52 +0600

    Increased height of product card to make it closer to the figma design

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -13,7 +13,7 @@ const ProductCard = ({ product }) => {
       <img
         src={product.thumbnail}
         alt={product.title}
-        className="w-full h-48 object-cover"
+        className="w-full object-cover"
       />
       <div className="p-4">
         <h3 className="text-lg font-medium mb-1">{product.title}</h3>
